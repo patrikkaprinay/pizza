d13c9354a3bc3b7c303a9d1285057a367211a4e6
Patrik Kaprinay <patrik.kaprinay@gmail.com>
2021-11-25 22:43:02 +0100

    added navbar animation

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,8 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { BiMenu } from 'react-icons/bi'
+import { motion, AnimatePresence } from 'framer-motion'
+import { IoMdPizza } from 'react-icons/io'
 
 const Navbar = () => {
   const [opened, setOpened] = useState(false)
@@ -14,11 +16,16 @@ const Navbar = () => {
   }, [])
 
   return (
-    <div className={`bg-gray-300 ${opened ? 'pt-4 pb-2' : 'py-4'}`}>
+    <motion.div className={`bg-gray-300 ${opened ? 'pt-4 pb-2' : 'py-4'}`}>
       <div className="container h-full sm:mx-auto grid grid-cols-5 px-4">
         <div className="col-span-4 md:col-span-1 flex items-center text-3xl">
-          <p className="w-max font-praise">
-            <Link href="/">Pizza Milano</Link>
+          <p className="w-full font-praise">
+            <Link href="/">
+              <p className="flex flex-row">
+                <IoMdPizza style={{ width: 27 }} />
+                Pizza Milano
+              </p>
+            </Link>
           </p>
         </div>
         <div className="md:col-span-3 md:flex justify-center items-center flex-row gap-8 hidden">
@@ -35,22 +42,29 @@ const Navbar = () => {
         >
           <BiMenu className="text-3xl" />
         </div>
-        {opened && (
-          <div className="col-span-12">
-            <Link href="/menu">
-              <p className="w-full flex justify-center items-center flex-col py-4 hover:cursor-pointer">
-                Menu
-              </p>
-            </Link>
-            <Link href="/kontakt">
-              <p className="w-full flex justify-center items-center flex-col py-4 hover:cursor-pointer">
-                Kontakt
-              </p>
-            </Link>
-          </div>
-        )}
+        <AnimatePresence>
+          {opened && (
+            <motion.div
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: 'auto' }}
+              exit={{ opacity: 0, height: 0 }}
+              className="col-span-12 overflow-hidden"
+            >
+              <Link href="/menu">
+                <p className="w-full flex justify-center items-center flex-col py-4 hover:cursor-pointer">
+                  Menu
+                </p>
+              </Link>
+              <Link href="/kontakt">
+                <p className="w-full flex justify-center items-center flex-col py-4 hover:cursor-pointer">
+                  Kontakt
+                </p>
+              </Link>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
