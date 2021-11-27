04395efa4342187eb341d2d9630a3e95474211a9
Patrik Kaprinay <patrik.kaprinay@gmail.com>
2021-11-27 20:37:58 +0100

    scrolling issue

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ import Welcome from '../components/Welcome'
 
 export default function Home() {
   return (
-    <div>
+    <div className="overflow-y-scroll">
       <Welcome />
       <Menu />
       <Contact />
