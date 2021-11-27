6c483a1a7d53660141c102ed310a4a3f2d88da77
Patrik Kaprinay <patrik.kaprinay@gmail.com>
2021-11-28 00:12:48 +0100

    font-weight

--- components/Announcement.jsx
+++ components/Announcement.jsx
@@ -3,7 +3,7 @@ import { GrAnnounce } from 'react-icons/gr'
 const Announcement = (props) => {
   return (
     <div className="w-full bg-black text-white py-3 px-2 text-xs xs:text-base">
-      <p className="font-roboto-s font-semibold text-center flex justify-center items-center gap-3">
+      <p className="font-roboto-s font-medium text-center flex justify-center items-center gap-3">
         <GrAnnounce className="transform invert" />
         {props.text}
         <GrAnnounce className="transform invert" />
