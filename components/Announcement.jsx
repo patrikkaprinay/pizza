136e4fa5a026ebeef3cce7650a4115c7d463f058
Patrik Kaprinay <patrik.kaprinay@gmail.com>
2021-11-28 00:01:40 +0100

    added announcement

---
+++ components/Announcement.jsx
@@ -0,0 +1,15 @@
+import { GrAnnounce } from 'react-icons/gr'
+
+const Announcement = (props) => {
+  return (
+    <div className="w-full bg-black text-white py-3 px-2 text-xs xs:text-base">
+      <p className="font-roboto-s font-semibold text-center flex justify-center items-center gap-3">
+        <GrAnnounce className="transform invert" />
+        {props.text}
+        <GrAnnounce className="transform invert" />
+      </p>
+    </div>
+  )
+}
+
+export default Announcement
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,50 +1,54 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { BiMenu } from 'react-icons/bi'
 import { motion, AnimatePresence } from 'framer-motion'
 import NavItems from './NavItems'
 import Logo from './Logo'
+import Announcement from '../components/Announcement'
 
 const Navbar = (props) => {
   const [opened, setOpened] = useState(false)
 
   return (
-    <div
-      className={`py-4 fixed w-full bg-white ${
-        props.scrolled || opened ? 'shadow-md' : ''
-      } transition duration-500 z-10`}
-    >
-      <div className="lg:container h-full sm:mx-auto flex justify-between items-center px-4">
-        <div className="col-span-4 md:col-span-2 flex items-center text-4xl md:text-3xl">
-          <a
-            href="#"
-            className="flex flex-row"
-            onClick={() => setOpened(false)}
+    <div className="fixed w-full z-10">
+      <Announcement text="Hľadáme rozvozcu/rozvozkyňu na víkendy!" />
+      <div
+        className={`py-4 w-full bg-white ${
+          props.scrolled || opened ? 'shadow-md' : ''
+        } transition duration-500`}
+      >
+        <div className="lg:container h-full sm:mx-auto flex justify-between items-center px-4">
+          <div className="col-span-4 md:col-span-2 flex items-center text-4xl md:text-3xl">
+            <a
+              href="#"
+              className="flex flex-row"
+              onClick={() => setOpened(false)}
+            >
+              <Logo start={true} />
+            </a>
+          </div>
+          <div className="md:col-span-3 md:flex justify-center items-center flex-row gap-2 lg:gap-8 hidden text-lg">
+            <NavItems />
+          </div>
+          <div
+            className="col-span-1 md:hidden flex justify-end items-center hover:cursor-pointer"
+            onClick={() => setOpened((e) => !e)}
           >
-            <Logo start={true} />
-          </a>
+            <BiMenu className="text-3xl" />
+          </div>
         </div>
-        <div className="md:col-span-3 md:flex justify-center items-center flex-row gap-2 lg:gap-8 hidden text-lg">
-          <NavItems />
-        </div>
-        <div
-          className="col-span-1 md:hidden flex justify-end items-center hover:cursor-pointer"
-          onClick={() => setOpened((e) => !e)}
-        >
-          <BiMenu className="text-3xl" />
-        </div>
+        <AnimatePresence>
+          {opened && (
+            <motion.div
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: 'auto' }}
+              exit={{ opacity: 0, height: 0 }}
+              className="overflow-hidden md:hidden text-xl"
+            >
+              <NavItems hideNav={() => setOpened(false)} />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
-      <AnimatePresence>
-        {opened && (
-          <motion.div
-            initial={{ opacity: 0, height: 0 }}
-            animate={{ opacity: 1, height: 'auto' }}
-            exit={{ opacity: 0, height: 0 }}
-            className="overflow-hidden md:hidden text-xl"
-          >
-            <NavItems hideNav={() => setOpened(false)} />
-          </motion.div>
-        )}
-      </AnimatePresence>
     </div>
   )
 }
